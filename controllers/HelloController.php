03c9704fe512e7ecf132fcee6f5b64389167a0bc
darren-livaway <darren.zhang@livaway.org>
2018-12-06 11:54:23 +0800

    test

---
+++ controllers/HelloController.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\controllers;
+
+use yii\console\Controller;
+use app\commands\BaseActiveController;
+use app\helpers\Helper;
+use app\models\Ad;
+use app\models\ForgetForm;
+use app\models\Syscode;
+use app\models\User;
+use Yii;
+/**
+ * This command echoes the first argument that you have entered.
+ *
+ * This command is provided as an example for you to learn how to create console commands.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class HelloController extends Controller
+{
+    /**
+     * This command echoes what you have entered as the message.
+     * @param string $message the message to be echoed.
+     */
+    public function actionIndex($message = 'hello world')
+    {
+        echo $message . "\n";
+    }
+
+
+
+}
